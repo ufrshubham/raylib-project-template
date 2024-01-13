217f6bc32030bddabc62e15f5eb32db5794d96f3
DevKage <ufrshubham@gmail.com>
2024-01-13 15:42:34 +0530

    Setup include and src directories

---
+++ include/Version.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+std::string GetVersion()
+{
+    return "1.0.0";
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Version.hpp"
+
+int main()
+{
+    std::cout << "Raylib Project Template v" << GetVersion() << std::endl;
+}
