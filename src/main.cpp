cf23b2aa0dc7ae20b61ccfdae8e35a184a1677e2
DevKage <ufrshubham@gmail.com>
2024-01-13 15:59:50 +0530

    Build and link with raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 #include "Version.hpp"
+#include "raylib.h"
 
 int main()
 {
     std::cout << "Raylib Project Template v" << GetVersion() << std::endl;
+    std::cout << "Based on Raylib v" << RAYLIB_VERSION << std::endl;
 }
